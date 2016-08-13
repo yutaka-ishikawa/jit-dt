5ca745748d575f2945cb3dff8b6e80596b8bbb8a
Ubuntu14 <yutaka.ishikawa@riken.jp>
2016-08-13 15:17:40 +0900

    correcting tar command

--- dist-src.sh
+++ dist-src.sh
@@ -1,2 +1,6 @@
-tar README src/Makefile src/http_daemon.c src/clk.c src/http_put.c src/watch_and_transfer.c src/tranlib.h
+#!/bin/bash
+#
+# git clone ishikawa@www.sys.aics.riken.jp:/var/aics-sys/git/jit-dt
+
+tar cf jit-dt-src.tar README src/Makefile src/http_daemon.c src/clk.c src/http_put.c src/watch_and_transfer.c src/tranlib.h
 
