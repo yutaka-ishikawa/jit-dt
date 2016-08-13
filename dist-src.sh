f73a4cfd297858cfe6ffc0546fcfd756693bfb1c
Ubuntu14 <yutaka.ishikawa@riken.jp>
2016-08-13 15:19:11 +0900

    file name was wrong

--- dist-src.sh
+++ dist-src.sh
@@ -2,5 +2,5 @@
 #
 # git clone ishikawa@www.sys.aics.riken.jp:/var/aics-sys/git/jit-dt
 
-tar cf jit-dt-src.tar README src/Makefile src/http_daemon.c src/clk.c src/http_put.c src/watch_and_transfer.c src/tranlib.h
+tar cf jit-dt-src.tar README src/Makefile src/http_daemon.c src/clk.c src/http_put.c src/watch_and_transfer.c src/translib.h src/translib.c
 
